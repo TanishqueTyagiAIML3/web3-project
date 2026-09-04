80e5c4a4cf5adebc3035c6a28c09ac048aabbd48
unknown <tanishque30@gmail.com>
2026-09-04 19:33:57 +0530

    fix error

--- lib.commonjs/_tests/utils.js
+++ lib.commonjs/_tests/utils.js
@@ -5,7 +5,7 @@ const tslib_1 = require("tslib");
 const fs_1 = tslib_1.__importDefault(require("fs"));
 const path_1 = tslib_1.__importDefault(require("path"));
 const zlib_1 = tslib_1.__importDefault(require("zlib"));
-exports.FAUCET_PRIVATEKEY = process.env.FAUCET_PRIVATEKEY || "MISSING_GITHUB_SECRET";
+exports.FAUCET_PRIVATEKEY = process.env.FAUCET_PRIVATEKEY || "";
 exports.INFURA_APIKEY = process.env.INFURA_APIKEY || "";
 exports.ALCHEMY_APIKEY = process.env.ALCHEMY_APIKEY || "";
 // Find the package root (based on the nyc output/ folder)
--- lib.esm/_tests/utils.js
+++ lib.esm/_tests/utils.js
@@ -1,7 +1,7 @@
 import fs from "fs";
 import path from "path";
 import zlib from "zlib";
-export const FAUCET_PRIVATEKEY = process.env.FAUCET_PRIVATEKEY || "MISSING_GITHUB_SECRET";
+export const FAUCET_PRIVATEKEY = process.env.FAUCET_PRIVATEKEY || "";
 export const INFURA_APIKEY = process.env.INFURA_APIKEY || "";
 export const ALCHEMY_APIKEY = process.env.ALCHEMY_APIKEY || "";
 // Find the package root (based on the nyc output/ folder)
